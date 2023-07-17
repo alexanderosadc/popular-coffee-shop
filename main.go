ef427db0ae963fb2c69df63e977fa51059cf15b2
alexanderosadc <alexanderosadc@gmail.com>
2023-07-17 18:30:58 +0300

    refactor: changes name of package infrastructure

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/alexanderosadc/popular-coffee-shop/pkg/infrastructure"
+	"github.com/alexanderosadc/popular-coffee-shop/pkg/handlers"
 )
 
 // init parses config files before start of the server
@@ -15,7 +15,7 @@ func init() {
 var port string = ":8080"
 
 func main() {
-	http.HandleFunc("/buycoffee", infrastructure.BuyCofee)
+	http.HandleFunc("/buycoffee", handlers.BuyCofee)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		fmt.Println("server is down because:" + err.Error())
 	}
