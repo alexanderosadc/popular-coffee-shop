f0dd347e16f871c65fc17e4757fb5cd43be17d1e
alexanderosadc <alexanderosadc@gmail.com>
2023-07-17 20:21:48 +0300

    feat: parse yaml file with cofee quotas

--- main.go
+++ main.go
@@ -3,13 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/alexanderosadc/popular-coffee-shop/pkg/handlers"
+	"gopkg.in/yaml.v3"
 )
+
+type CofeeLimit struct {
+	Time  string `yaml:"time"`
+	Limit int    `yaml:"limit"`
+}
+type Quota struct {
+	QuotaName    string                `yaml:"quota_name"`
+	TypesOfCofee map[string]CofeeLimit `yaml:"types_of_cofee"`
+}
 
 // init parses config files before start of the server
 func init() {
+	dat, err := os.ReadFile("config/cofee_shop_quotas.yaml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	yamlConf := []Quota{}
+
+	if err := yaml.Unmarshal(dat, &yamlConf); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(yamlConf)
 }
 
 var port string = ":8080"
