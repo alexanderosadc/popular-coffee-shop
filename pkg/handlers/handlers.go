8f3719aa7530ff1abfb9681e9ba0945c19c8d16c
alexanderosadc <alexanderosadc@gmail.com>
2023-07-22 16:04:46 +0300

    feat: adds error handling of 422 error

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/alexanderosadc/popular-coffee-shop/pkg/app"
 )
@@ -11,14 +12,20 @@ func BuyCofee(w http.ResponseWriter, r *http.Request, bl *app.CofeeBL) {
 	membership, err := bl.GetMembershipType(userMembership)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	cofeeType := r.URL.Query().Get("cofeeType")
 	if err := bl.ValidateCofeeType(cofeeType, membership); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	if err := bl.ProcessCofeeReq(r.Header.Get("user-id"), cofeeType, userMembership, membership.TypesOfCofee); err != nil {
+		if strings.Contains(err.Error(), "422") {
+			http.Error(w, err.Error(), http.StatusTooManyRequests)
+			return
+		}
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 }
